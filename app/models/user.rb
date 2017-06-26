d05d741d6592c57cd322f84698116ea442de22a6
Wolf134 <wolf6285@gmail.com>
2017-06-26 16:09:17 +0300

    gem 'devise' cofiguret

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :authentication_keys => [:login]
+   	# Virtual attribute for authenticating by either username or email
+  	# This is in addition to a real persisted field like 'username'
+  	attr_accessor :login
+
+  	def self.find_for_database_authentication(warden_conditions)
+      	conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+  	# Only allow letter, number, underscore and punctuation.
+	validates :username,
+	  		  :presence => true,
+	  		  :uniqueness => {
+	          :case_sensitive => false
+	  } # etc.
+	  
+	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+	validate :validate_username
+
+	def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:username, :invalid)
+	  end
+	end
+end
